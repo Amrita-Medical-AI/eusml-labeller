f03367740bbc5bfe1ca85af6519dd153b40efcb8
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2023-04-03 12:48:57 +0530

    style: update style on label page

--- app/routes/patient.label.$mrd.jsx
+++ app/routes/patient.label.$mrd.jsx
@@ -2,7 +2,6 @@ import { useLoaderData } from "@remix-run/react";
 import { getPatientByMRD } from '~/models/patient.server';
 import { json } from "@remix-run/node";
 
-
 export const loader = async ({ request, params }) => {
     const patientMRD = params.mrd 
     const patient = await getPatientByMRD({ patientMRD });
@@ -12,9 +11,21 @@ export const loader = async ({ request, params }) => {
     return json({ patient });
 }
 
-export default function Label(){
+export default function Label() {
     const data = useLoaderData();
     return (
-        <h1>Label {data.patient.name} data here</h1>
+        <div className="flex flex-row justify-center items-start min-h-screen bg-slate-600">
+            <h1 className="text-4xl font-bold text-center text-white p-2">
+                MRD
+            </h1>
+            <h1 className="text-4xl font-bold text-center text-teal-400 p-2">
+                {data.patient.mrd}
+            </h1>
+            {/* display other patient data here */}
+        </div>
     );
 }
+
+
+
+
