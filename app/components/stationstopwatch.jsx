c24a4e7ed6a1d8a094d01068f9d94a5316471da3
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2023-04-10 15:27:44 +0530

    feat: once a station timer is stopped it should be frozen

--- app/components/stationstopwatch.jsx
+++ app/components/stationstopwatch.jsx
@@ -1,4 +1,3 @@
-// StationStopwatch.js
 import { useState, useEffect } from "react";
 import { PlayIcon, StopIcon } from "@heroicons/react/solid";
 import { formatTime } from "./utils";
@@ -7,13 +6,15 @@ export default function StationStopwatch({ endTime, stationName }) {
   const [stationStartTime, setStationStartTime] = useState(0);
   const [stationEndTime, setStationEndTime] = useState(0);
   const [stationTimer, setStationTimer] = useState(null);
+  const [isStopped, setIsStopped] = useState(false);
 
   const toggleStationTimer = () => {
     if (stationTimer) {
       clearInterval(stationTimer);
       setStationTimer(null);
+      setIsStopped(true);
     } else {
-      setStationStartTime(endTime); // set station start time here
+      setStationStartTime(endTime);
       setStationEndTime(endTime);
       setStationTimer(
         setInterval(() => setStationEndTime((prevTime) => prevTime + 1), 100)
@@ -28,24 +29,21 @@ export default function StationStopwatch({ endTime, stationName }) {
       }
     };
   }, [stationTimer]);
+
   return (
     <div className="flex flex-col items-center gap-2">
-      <div className="flex h-14 w-48 flex-row items-center justify-center gap-4 rounded bg-teal-400 p-2">
-        <span className="font-mono text-lg text-white">
-          {formatTime(stationEndTime)}
-        </span>
-      </div>
       <div className="flex flex-row items-center rounded bg-blue-500 p-2">
         <label
           className="text-3xl text-white"
           style={{ alignSelf: "flex-start" }}
         >
-          {stationTimer ? `Stop ${stationName} Timer` : `Start ${stationName} Timer`}
+          {isStopped ? formatTime(stationEndTime) : stationTimer ? formatTime(stationEndTime) : `Start ${stationName} Timer`}
         </label>
         <button
           type="button"
           onClick={toggleStationTimer}
           className="ml-2 rounded-full bg-blue-400 p-1 hover:bg-blue-500 focus:bg-blue-300"
+          disabled={isStopped}
         >
           {stationTimer ? (
             <StopIcon className="h-6 w-6 text-white" />
