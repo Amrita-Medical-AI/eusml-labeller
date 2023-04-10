cd5ae5b9580c64177194a08faa98dc91074784f7
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2023-04-10 13:14:49 +0530

    fix: remove logout check on notes page

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -13,9 +13,8 @@ describe("smoke tests", () => {
     cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
     cy.findByLabelText(/password/i).type(loginForm.password);
     cy.findByRole("button", { name: /create account/i }).click();
+    
+    //@todo add a check for logout on patient page
 
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByRole("button", { name: /logout/i }).click();
-    cy.findByRole("link", { name: /log in/i });
   });
 });
