ccba83a0aeb17dba2659ac2ea7a17e4d43c22bcd
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2023-04-11 12:51:02 +0530

    style: move FNA stopwatch to different component

---
+++ app/components/FNAstopwatch.jsx
@@ -0,0 +1,80 @@
+// FNAStopwatch.js
+import { useState, useEffect } from "react";
+import { PlayIcon, StopIcon } from "@heroicons/react/solid";
+import { formatTime } from "./utils";
+
+export default function FNAStopwatch({ endTime, procedureStarted }) {
+  const [stationStartTime, setStationStartTime] = useState(0);
+  const [stationEndTime, setStationEndTime] = useState(0);
+  const [stationTimer, setStationTimer] = useState(null);
+  const [timePeriods, setTimePeriods] = useState([]);
+
+  const toggleStationTimer = () => {
+    if (stationTimer) {
+      clearInterval(stationTimer);
+      setStationTimer(null);
+      setStationEndTime((prevTime) => prevTime + 1);
+      setTimePeriods([
+        ...timePeriods,
+        { start: stationStartTime, end: stationEndTime + 1 },
+      ]);
+    } else {
+      setStationStartTime(endTime);
+      setStationEndTime(endTime);
+      setStationTimer(
+        setInterval(() => setStationEndTime((prevTime) => prevTime + 1), 100)
+      );
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (stationTimer) {
+        clearInterval(stationTimer);
+      }
+    };
+  }, [stationTimer]);
+
+  let isDisabled = !procedureStarted;
+
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-row items-center rounded bg-slate-600 p-2">
+        <div className="relative mx-4 flex h-14 w-96 items-center justify-center">
+          <label
+            className="absolute left-0 right-0 text-center text-3xl text-white"
+            style={{ alignSelf: "flex-start" }}
+          >
+            {stationTimer
+              ? formatTime(stationEndTime)
+              : `Start FNA Timer`}
+          </label>
+          <button
+            type="button"
+            onClick={toggleStationTimer}
+            className={`absolute right-0 ml-2 rounded-full p-1 ${isDisabled ? 'bg-gray-400' : 'bg-blue-400 hover:bg-blue-500 focus:bg-blue-300'}`}
+            disabled={isDisabled}
+          >
+            {stationTimer ? (
+              <StopIcon className="h-6 w-6 text-white" />
+            ) : (
+              <PlayIcon className="h-6 w-6 text-white" />
+            )}
+          </button>
+          {timePeriods.map((period, index) => (
+            <div key={index} className="hidden">
+              <input
+                name={`Start FNA ${index + 1}`}
+                value={period.start}
+              />
+              <input
+                name={`Stop FNA ${index + 1}`}
+                value={period.end}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/components/stopwatch.jsx
+++ app/components/stopwatch.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import ProcedureStopwatch from "./procedurestopwatch";
 import StationStopwatch from "./stationstopwatch";
+import FNAStopwatch from "./FNAstopwatch";
 
 export default function Stopwatch() {
   const [initialTime, setInitialTime] = useState(0);
   const [runningStation, setRunningStation] = useState(null);
-  const [procedureStarted, setProcedureStarted] = useState(false); // Add this line
+  const [procedureStarted, setProcedureStarted] = useState(false);
 
   return (
     <div className="flex flex-col items-center gap-2 mt-5">
@@ -13,7 +14,7 @@ export default function Stopwatch() {
       <StationStopwatch endTime={initialTime} stationName="Station 1" runningStation={runningStation} setRunningStation={setRunningStation} procedureStarted={procedureStarted} />
       <StationStopwatch endTime={initialTime} stationName="Station 2" runningStation={runningStation} setRunningStation={setRunningStation} procedureStarted={procedureStarted} />
       <StationStopwatch endTime={initialTime} stationName="Station 3" runningStation={runningStation} setRunningStation={setRunningStation} procedureStarted={procedureStarted} />
-      <StationStopwatch endTime={initialTime} stationName="FNA" procedureStarted={procedureStarted} />
+      <FNAStopwatch endTime={initialTime} procedureStarted={procedureStarted} />
     </div>
   );
 }
