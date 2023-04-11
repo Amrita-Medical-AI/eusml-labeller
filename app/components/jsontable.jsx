2ae9a6d32b21c438526481182d5c31582c0e6ef7
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2023-04-11 17:17:29 +0530

    style: make the table scrollable for mobile view

--- app/components/jsontable.jsx
+++ app/components/jsontable.jsx
@@ -27,29 +27,32 @@ export default function JsonTable({ data }) {
   });
   
     return (
-      <div className="flex items-center justify-center min-h-screen">
-      <table className="table-auto border-collapse border border-gray-300">
-        <thead>
-          <tr>
-            {keys.map((key, index) => (
-              <th key={index} className="border border-gray-300 px-4 py-2 bg-teal-500 text-white text-3xl">
-                {key === 'patientName' ? 'Patient Name' : key}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {Array.from({ length: Math.max(...keys.map((key) => groupedData[key].length)) }).map((_, rowIndex) => (
-            <tr key={rowIndex}>
-              {keys.map((key, cellIndex) => (
-                <td key={cellIndex} className="border border-gray-300 px-4 py-2 bg-slate-700 text-white text-2xl text-center">
-                  {groupedData[key][rowIndex]}
-                </td>
+      <div className="w-full mt-5 mb-1 flex justify-center">
+        <div className="w-full overflow-x-auto md:overflow-x-visible">
+          <table className="table-auto min-w-full text-center bg-white rounded-md overflow-hidden">
+          <thead>
+            <tr>
+              {keys.map((key, index) => (
+                <th key={index} className="border border-gray-300 px-4 md:px-2 py-2 bg-teal-500 text-white text-xl">
+                  {key === 'patientName' ? 'Patient Name' : key}
+                </th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {Array.from({ length: Math.max(...keys.map((key) => groupedData[key].length)) }).map((_, rowIndex) => (
+              <tr key={rowIndex}>
+                {keys.map((key, cellIndex) => (
+                  <td key={cellIndex} className="border border-gray-300 px-4 md:px-2 py-2 bg-slate-700 text-white text-2xl text-center">
+                    {groupedData[key][rowIndex]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       </div>
     );
+    
   }
--- app/routes/patient.$mrd.jsx
+++ app/routes/patient.$mrd.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, Link } from "@remix-run/react";
 import { json } from "@remix-run/node";
 import {
   getPatientByMRD,
@@ -15,24 +15,40 @@ export const loader = async ({ request, params }) => {
   }
   return json({ patient, procedureTimeStamps });
 };
+
 export default function Patient() {
   const data = useLoaderData();
   const timeStamps = data.procedureTimeStamps;
+
   return (
-    <div className="flex min-h-screen flex-row items-start justify-center bg-sky-900">
+    <div className="w-full max-w-screen-md md:max-w-screen-lg mx-auto overflow-x-auto md:overflow-x-visible">
+     <div className="relative min-h-screen flex flex-col items-center justify-center bg-sky-900">
       <div className="flex w-full flex-col items-center">
         <div className="flex w-full flex-row items-center justify-center bg-slate-700 p-2">
-        <h1 className="p-2 text-center text-4xl font-bold text-white">MRD: </h1>
-          <h1 className="p-2 text-center text-4xl font-bold text-teal-400">
+          <h1 className="p-2 text-center text-2xl font-bold text-white">
+            MRD:{" "}
+          </h1>
+          <h1 className="p-2 text-center text-2xl font-bold text-teal-400">
             {data.patient.mrd}
           </h1>
-          <h1 className="p-2 text-center text-4xl font-bold text-white">Name: </h1>
-          <h1 className="p-2 text-center text-4xl font-bold text-teal-400">
+          <h1 className="p-2 text-center text-2xl font-bold text-white">
+            Name:{" "}
+          </h1>
+          <h1 className="p-2 text-center text-2xl font-bold text-teal-400">
             {data.patient.name}
-        </h1>
+          </h1>
+        </div>
+        <div className="flex min-h-screen w-full flex-col items-center justify-center">
+          <JsonTable data={timeStamps} />
+          <Link
+            to="/"
+            className="mt-5 mb-1 inline-block rounded bg-teal-400 px-6 py-2 text-sm text-white hover:bg-slate-700 md:text-base"
+          >
+            Label another patient
+          </Link>
+        </div>
       </div>
-      <JsonTable data={timeStamps} />
-      </div>
+    </div>
     </div>
   );
 }
