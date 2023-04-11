85e55b49f86e0044153cf99b81e218b15cde746a
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2023-04-11 16:13:45 +0530

    feat: style json table

--- app/components/jsontable.jsx
+++ app/components/jsontable.jsx
@@ -1,53 +1,48 @@
 export default function JsonTable({ data }) {
-    // Initialize an empty object to store the grouped data
     const groupedData = {};
   
-    // Iterate over the keys in the data object
     Object.keys(data).forEach((key) => {
-      // Define a regular expression to match keys like "Start Station 1 1" and "Stop FNA 1 1"
+      if(key === "pk") return;
       const regex = /^(.+)\s\d+$/;
   
-      // Test the key against the regular expression
       const match = key.match(regex);
   
-      // If the key matches the regular expression
       if (match) {
-        // Extract the group name, e.g., "Start Station 1" or "Stop FNA 1"
         const groupName = match[1];
   
-        // If the group name is not already a key in the groupedData object, initialize an empty array for it
         if (!groupedData[groupName]) {
           groupedData[groupName] = [];
         }
   
-        // Append the value for the current key to the array for the group name
         groupedData[groupName].push(data[key]);
       } else {
-        // If the key does not match the regular expression, add it directly to the groupedData object
         groupedData[key] = [data[key]];
       }
     });
   
-    // Generate an array of keys from the groupedData object
-    const keys = Object.keys(groupedData);
+  const keys = Object.keys(groupedData).sort((a, b) => {
+    if (a === 'patientName') return -1;
+    if (b === 'patientName') return 1;
+    return 0;
+  });
   
     return (
+      <div className="flex items-center justify-center min-h-screen">
       <table className="table-auto border-collapse border border-gray-300">
         <thead>
           <tr>
             {keys.map((key, index) => (
-              <th key={index} className="border border-gray-300 px-4 py-2">
-                {key}
+              <th key={index} className="border border-gray-300 px-4 py-2 bg-teal-500 text-white text-3xl">
+                {key === 'patientName' ? 'Patient Name' : key}
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {/* Calculate the maximum number of rows needed to display all values in the groupedData object */}
           {Array.from({ length: Math.max(...keys.map((key) => groupedData[key].length)) }).map((_, rowIndex) => (
             <tr key={rowIndex}>
               {keys.map((key, cellIndex) => (
-                <td key={cellIndex} className="border border-gray-300 px-4 py-2">
+                <td key={cellIndex} className="border border-gray-300 px-4 py-2 bg-slate-700 text-white text-2xl text-center">
                   {groupedData[key][rowIndex]}
                 </td>
               ))}
@@ -55,5 +50,6 @@ export default function JsonTable({ data }) {
           ))}
         </tbody>
       </table>
+      </div>
     );
   }
--- app/routes/patient.$mrd.jsx
+++ app/routes/patient.$mrd.jsx
@@ -19,9 +19,20 @@ export default function Patient() {
   const data = useLoaderData();
   const timeStamps = data.procedureTimeStamps;
   return (
-    <div>
-      <h1>Show MRD: {data.patient.mrd} Time Stamps</h1>
+    <div className="flex min-h-screen flex-row items-start justify-center bg-sky-900">
+      <div className="flex w-full flex-col items-center">
+        <div className="flex w-full flex-row items-center justify-center bg-slate-700 p-2">
+        <h1 className="p-2 text-center text-4xl font-bold text-white">MRD: </h1>
+          <h1 className="p-2 text-center text-4xl font-bold text-teal-400">
+            {data.patient.mrd}
+          </h1>
+          <h1 className="p-2 text-center text-4xl font-bold text-white">Name: </h1>
+          <h1 className="p-2 text-center text-4xl font-bold text-teal-400">
+            {data.patient.name}
+        </h1>
+      </div>
       <JsonTable data={timeStamps} />
+      </div>
     </div>
   );
 }
