cb8dc3ecabae56b6a1d4b308a9b74893d1ec24f3
Anshuman Swain <anshuman8swain@gmail.com>
2023-08-26 23:35:07 +0530

    feat:morphology update

---
+++ app/components/updateMorphology.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const UpdateMorphologyModal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      <div className="absolute inset-0 bg-gray-800 opacity-70"></div>
+      <div className="z-10 rounded-lg bg-background p-4">
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateMorphologyModal;
