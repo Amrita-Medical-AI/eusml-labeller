275b05af10ceccd6c2a2886d3327e56e3dc97484
Anshuman Swain <anshuman8swain@gmail.com>
2024-01-15 11:06:32 +0530

    feat:update morphology form

--- app/routes/patient.$id.jsx
+++ app/routes/patient.$id.jsx
@@ -7,7 +7,6 @@ import {
   updatePatientDetails,
 } from "~/models/patient.server";
 import JsonTable from "~/components/jsontable";
-import PopupModal from "~/components/popupModal";
 
 export const action = async ({ request, params }) => {
   let formData = await request.formData();
@@ -43,15 +42,16 @@ export const loader = async ({ request, params }) => {
 
 export default function Patient() {
   const data = useLoaderData();
-  const [modalOpen, setModalOpen] = React.useState(false);
+  const [showSaveMorphology, setShowSaveMorphology] = React.useState(false);
   const timeStamps = data.procedureTimeStamps;
   const [selectedMorphology, setSelectedMorphology] = React.useState(
     data?.patient?.morphology
   );
 
-  const toggleModal = () => {
-    setModalOpen(!modalOpen);
+  const saveMorphology = () => {
+    setShowSaveMorphology(false);
   };
+
   const morphologyOptions = ["Normal", "CCP", "Cysts", "Mass", "Others"];
 
   const handleCheckboxChange = (option) => {
@@ -66,48 +66,12 @@ export default function Patient() {
         ? selectedMorphology.filter((selected) => selected !== option)
         : [...selectedMorphology, option];
     }
-
+    setShowSaveMorphology(true);
     setSelectedMorphology(updatedOptions);
   };
 
   return (
     <div className="mx-auto w-full  bg-sky-900">
-      <PopupModal isOpen={modalOpen} onClose={toggleModal}>
-        <Form method="post" onSubmit={toggleModal}>
-          <label className="mx-5 mt-5 text-2xl text-teal-400">
-            Update Morphology
-          </label>
-          <div className="my-4 flex flex-col gap-2 text-white">
-            {morphologyOptions.map((option) => (
-              <label key={option} className="flex items-center gap-3">
-                <input
-                  type="checkbox"
-                  name="morphology"
-                  value={option}
-                  checked={selectedMorphology.includes(option)}
-                  onChange={() => handleCheckboxChange(option)}
-                  className=" rounded-md border-teal-500 bg-teal-50 p-4 text-2xl font-medium text-teal-700 focus:ring-teal-200"
-                />
-                {option}
-              </label>
-            ))}
-          </div>
-          <div className="flex flex-row justify-between">
-            <button
-              className=" m-3 rounded-md bg-red-500 px-4 py-3 text-gray-100 hover:bg-red-600 hover:text-gray-300"
-              onClick={toggleModal}
-            >
-              Close
-            </button>
-            <button
-              className=" m-3 rounded-md bg-teal-600 px-4 py-3 text-gray-100 hover:bg-teal-800 hover:text-gray-300"
-              type="submit"
-            >
-              Update
-            </button>
-          </div>
-        </Form>
-      </PopupModal>
       <div className="relative flex min-h-screen flex-col items-center justify-center bg-background">
         <div className="flex w-full flex-col items-center ">
           <div className="flex w-full flex-row items-center justify-center bg-slate-700 p-2">
@@ -116,7 +80,7 @@ export default function Patient() {
             </div>
           </div>
           <div className="flex min-h-screen w-full flex-col items-start justify-start px-5">
-            <div className="my-8 mx-2 md:mx-5">
+            <div className="mx-2 my-8 md:mx-5">
               <div className="p-2 text-xl font-semibold text-white">
                 MRD:{" "}
                 <span className="p-2 text-xl font-bold text-teal-400">
@@ -145,17 +109,41 @@ export default function Patient() {
                   {data?.patient?.morphology?.join(", ")}
                 </span>
               </div>
-              <button
-                onClick={toggleModal}
-                className="mx-2 mt-5 mb-1 inline-block rounded bg-teal-600 px-6 py-2 text-base text-white shadow-lg hover:bg-slate-700 md:text-lg"
-              >
-                Update Morphology
-              </button>
+              <div className="my-3">
+                <label className="mx-2 mt-5 text-xl font-semibold text-white">
+                  Update Morphology
+                </label>
+                  <Form method="post" onSubmit={saveMorphology}>
+                <div className="mx-5 my-4 flex flex-col gap-2 text-white">
+                  {morphologyOptions.map((option) => (
+                    <label key={option} className="flex items-center gap-3">
+                      <input
+                        type="checkbox"
+                        name="morphology"
+                        value={option}
+                        checked={selectedMorphology.includes(option)}
+                        onChange={() => handleCheckboxChange(option)}
+                        className=" rounded-md border-teal-500 bg-teal-50 p-4 text-2xl font-medium text-teal-700 focus:ring-teal-200"
+                      />
+                      {option}
+                    </label>
+                  ))}
+                </div>
+                {showSaveMorphology && (
+                    <button
+                    type="submit"
+                      className="mx-5 mb-1 mt-2 inline-block rounded bg-teal-600 px-6 py-2 text-base text-white shadow-lg hover:bg-slate-700 md:text-lg transition duration-150 ease-in-out"
+                    >
+                      Save Morphology
+                    </button>
+                )}
+                </Form>
+              </div>
             </div>
             <JsonTable data={timeStamps} />
             <Link
               to="/"
-              className="mt-5 mb-1 inline-block rounded bg-dark-blue px-8 py-3 text-base text-white hover:bg-slate-700 md:text-lg"
+              className="mb-1 mt-5 inline-block rounded bg-dark-blue px-8 py-3 text-base text-white hover:bg-slate-700 md:text-lg"
             >
               Label another patient
             </Link>
