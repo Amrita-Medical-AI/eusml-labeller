8e33c536685d51080e034d4fdd096b3996cb6753
anshuman <anshuman8swain@gmail.com>
2024-04-06 00:18:27 +0530

    feat:update biopsy from search

--- app/components/PatientDataCard.jsx
+++ app/components/PatientDataCard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Form } from "@remix-run/react";
 
 export default function PatientDataCard({ patient }) {
   const { patientId, mrd, name, date, doctor } = patient;
+
+  const [cancerValue, setCancerValue] = React.useState("");
+  const [biopsyValue, setBiopsyValue] = React.useState("");
 
   return (
     <div className="mx-auto max-w-lg rounded-md bg-white p-4 shadow-lg">
@@ -22,12 +26,10 @@ export default function PatientDataCard({ patient }) {
         <p className="text-gray-800">
           <span className="font-bold">Date : </span>
           <span>
-            {new Date(date).toLocaleDateString(
-              "en-IN",
-              {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
+            {new Date(date).toLocaleDateString("en-IN", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
             })}
           </span>
         </p>
@@ -37,18 +39,58 @@ export default function PatientDataCard({ patient }) {
         </p>
       </div>
       <div className=" flex space-x-5">
-
-      <button className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700" onClick={
-        () => navigator.clipboard.writeText(patientId)
-      }>
-        Copy UUID
-      </button>
-      <button className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700" onClick={
-        () => navigator.clipboard.writeText(mrd)
-      }>
-        Copy MRD
-      </button>
+        <button
+          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={() => navigator.clipboard.writeText(patientId)}
+        >
+          Copy UUID
+        </button>
+        <button
+          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={() => navigator.clipboard.writeText(mrd)}
+        >
+          Copy MRD
+        </button>
       </div>
+      <hr className="my-4" />
+      <div className="my-4 text-xl font-semibold"> Patient Report </div>
+      <Form method={"post"}>
+        <input type="hidden" name="intent" value={"biopsy"} />
+        <input type="hidden" name="patientId" value={patientId} />
+        <div className="my-2 p-2">
+          <label>
+            Cancer:
+            <select
+              className="my-1 w-full rounded border border-gray-300 p-2"
+              name="cancer"
+              value={cancerValue}
+              onChange={(e) => setCancerValue(e.target.value)}
+            >
+              <option value="">Select...</option>
+              <option value="true">Yes - Cancer</option>
+              <option value="false">No - Not Cancer</option>
+              <option value=" ">No Data</option>
+            </select>
+          </label>
+        </div>
+        <div className="my-2 p-2">
+          <div>
+            Biopsy Report:
+            <textarea
+              className="my-1 w-full rounded border border-gray-300 p-2"
+              name="biopsy"
+              value={biopsyValue}
+              onChange={(e) => setBiopsyValue(e.target.value)}
+            />
+          </div>
+        </div>
+        <button
+          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          type="submit"
+        >
+          Submit
+        </button>
+      </Form>
     </div>
   );
 }
